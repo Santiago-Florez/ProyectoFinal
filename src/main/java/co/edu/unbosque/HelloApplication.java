5d1267dcddcaeed56eb710219c4249f467674f45
OscarFlorez <66635225+Santiago-Florez@users.noreply.github.com>
2021-11-24 15:31:20 -0500

    Build Project

--- src/main/java/co/edu/unbosque/HelloApplication.java
+++ src/main/java/co/edu/unbosque/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.proyecto;
+package co.edu.unbosque;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
--- src/main/java/co/edu/unbosque/HelloResource.java
+++ src/main/java/co/edu/unbosque/HelloResource.java
@@ -1,4 +1,4 @@
-package com.example.proyecto;
+package co.edu.unbosque;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
