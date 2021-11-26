7392860a589a0b2507255001f050ff41647f20d6
OscarFlorez <66635225+Santiago-Florez@users.noreply.github.com>
2021-11-25 23:21:20 -0500

    Funcionalidad createdOwner.html Listas

--- target/Proyecto-1.0-SNAPSHOT/js/scriptCreateOwner.js
+++ target/Proyecto-1.0-SNAPSHOT/js/scriptCreateOwner.js
@@ -1,3 +1,40 @@
 document.getElementById("registrarOwner").onclick = function(){
-    console.log("gg")
+    let userName = document.getElementById("userProp").value
+    let email = document.getElementById("emailProp").value
+    let passWord = document.getElementById("passwordProp").value
+    let personId = document.getElementById("idProp").value
+    let name = document.getElementById("nombreProp").value
+    let address = document.getElementById("addressProp").value
+    let vecindario = document.getElementById("localidadProp").value
+
+    var ownerJson ={
+        "username":document.getElementById("userProp").value,
+        "email":document.getElementById("emailProp").value,
+        "password":document.getElementById("passwordProp").value,
+        "personId":document.getElementById("idProp").value,
+        "name":document.getElementById("nombreProp").value,
+        "address":document.getElementById("addressProp").value,
+        "neighborhood":document.getElementById("localidadProp").value
+    }
+
+    if(userName === "" || email === "" || passWord === "" || personId == null || name === "" || address === "" || vecindario === ""){
+        var elementP = document.getElementById("MensajeError").innerHTML = "Debe ingresar datos del Usuario para registrarlo" ;
+    }else{
+        fetch("http://localhost:8080/Taller5-1.0-SNAPSHOT/api/owners",{
+            method:"POST",
+            body: JSON.stringify(ownerJson), // enviar el JSON para la API
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            window.location.reload;
+        })
+        var elementP = document.getElementById("MensajeError");
+        elementP.innerHTML = "";
+    }
+
+    
 }
